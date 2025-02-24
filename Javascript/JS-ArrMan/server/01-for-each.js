97d7a04df18843d02cd23f376cdeba96bdca7e0c
Sannti <santiagocamejo21@gmail.com>
2025-02-24 19:03:46 -0300

    Comienzo curso de manipulacion de arrays

---
+++ Javascript/JS-ArrMan/server/01-for-each.js
@@ -0,0 +1,6 @@
+const letters = ['A', 'B', 'C', 'D'];
+
+for (let index = 0; index < letters.length; index++) {
+    const element = letters[index];
+    console.log(element + " elemento en la posicion " + (index + 1));
+}
