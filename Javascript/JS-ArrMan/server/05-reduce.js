e43d774483725e6bfe8f6562fbbd73709ddae637
Sannti <santiagocamejo21@gmail.com>
2025-03-10 20:17:18 -0300

    Reduce

---
+++ Javascript/JS-ArrMan/server/05-reduce.js
@@ -0,0 +1,11 @@
+const totales = [1,2,3,4,5];
+
+let sum = 0
+
+for (let index = 0; index < totales.length; index++) {
+    const element = totales[index];
+    sum = sum + element;
+}
+console.log(sum);
+console.log(element);
+
